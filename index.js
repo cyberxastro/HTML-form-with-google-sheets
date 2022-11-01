39d7ea5bf072ab11b06e5ce02330ea20014065fe
Gaurav Sharad Suryawanshi <109857735+astroxhacker@users.noreply.github.com>
2022-11-01 17:00:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbycVhriTWXlRzHPZzfLYE4u8CDB33u7vpDSdhjW13fkWxjNLvNH4ihECSGO8t4XsDoxhA/exec'
+const scriptURL = 'YOUR_GOOGLE _SHEETS_SCRIPT_url'
                 const form = document.forms['google-sheet']
-
+// Adding the event listner for the submit button, as soon as a user clicks on the below submit button, the below block of code will get executed.
                 form.addEventListener('submit', e => {
                     e.preventDefault()
                     fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+//                   After receving the form, user will see the below message in the browser as alert.
                         .then(response => alert("Thanks for Contacting us..! We Will Contact You Soon..."))
+//                   If any problem occurs, the user will see the below message.
                         .catch(error => console.error('Error!', error.message))
                 })
